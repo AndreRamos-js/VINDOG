91b893cdc1c150c1b3a5ecaf558d7d044b58514b
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-06-26 00:44:12 -0300

    Alterações
    -Alterando o método de filtragem da página de relatório de Jquery para o padrão list view.

--- Vindog/racas/views.py
+++ Vindog/racas/views.py
@@ -1,8 +1,7 @@
-from django.views.generic import CreateView, TemplateView
-from .forms import RacaForm
-from .models import Raca
-from .forms import CachorroForm
-from .models import Cachorro
+from django.views.generic import CreateView, ListView, TemplateView
+from django.urls import reverse_lazy
+from .forms import RacaForm, CachorroForm
+from .models import Raca, Cachorro
 
 
 
@@ -16,6 +15,16 @@ class TemplateRelatorioView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['racas'] = Raca.objects.all()
         context['cachorros'] = Cachorro.objects.all()
+
+        search_racas = self.request.GET.get('search_racas')
+        search_cachorros = self.request.GET.get('search_cachorros')
+
+        if search_racas:
+            context['racas'] = Raca.objects.filter(nome__icontains=search_racas)
+
+        if search_cachorros:
+            context['cachorros'] = Cachorro.objects.filter(nome__icontains=search_cachorros)
+
         return context
 
 class CadastrarRacaCreateView(CreateView):
