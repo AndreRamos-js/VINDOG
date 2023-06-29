947fc3b2757debd5423523ebe99b02cbaa21a63a
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-06-29 12:48:11 -0300

    Alterações
    -Separação da pagina relatório, agora raças e cachorros cadastrados terram suas próprias rotas.

--- Vindog/Vindog/urls.py
+++ Vindog/Vindog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from racas.views import CadastrarRacaCreateView, CadastrarCachorroCreateView,\
-TemplateIndexView, TemplateRelatorioView
+TemplateIndexView, TemplateRelatorioRacaView, TemplateRelatorioCachorroView
 
 
 
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^$', TemplateIndexView.as_view(), name='index'),
     url(r'^cadastrar_raca/$', CadastrarRacaCreateView.as_view(), name='cadastrar_raca'),
     url(r'^cadastrar_cachorro/$', CadastrarCachorroCreateView.as_view(), name='cadastrar_cachorro'),
-    url(r'^relatorio/$', TemplateRelatorioView.as_view(), name='relatorio'),
+    url(r'^relatorio_raca/$', TemplateRelatorioRacaView.as_view(), name='relatorio_raca'),
+    url(r'^relatorio_cachorro/$', TemplateRelatorioCachorroView.as_view(), name='relatorio_cachorro'),
 ]
--- Vindog/racas/views.py
+++ Vindog/racas/views.py
@@ -9,19 +9,28 @@ from django.http import HttpResponseRedirect
 class TemplateIndexView(TemplateView):
     template_name = 'index.html'
 
-class TemplateRelatorioView(TemplateView):
-    template_name = 'relatorio.html'
+class TemplateRelatorioRacaView(TemplateView):
+    template_name = 'relatorio_raca.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['racas'] = Raca.objects.all()
-        context['cachorros'] = Cachorro.objects.all()
 
         search_racas = self.request.GET.get('search_racas')
-        search_cachorros = self.request.GET.get('search_cachorros')
 
         if search_racas:
             context['racas'] = Raca.objects.filter(nome__icontains=search_racas)
+
+        return context
+    
+class TemplateRelatorioCachorroView(TemplateView):
+    template_name = 'relatorio_cachorro.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cachorros'] = Cachorro.objects.all()
+
+        search_cachorros = self.request.GET.get('search_cachorros')
 
         if search_cachorros:
             context['cachorros'] = Cachorro.objects.filter(nome__icontains=search_cachorros)
